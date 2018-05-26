c9a6f9e2a731c2d77cfd69b714530142f245f305
Ettore Stark <ettoreastark@gmail.com>
2018-05-26 11:21:28 -0400

    Nothing to say

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<link rel="stylesheet" href="css/main.css"/>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+		<title>Split button</title>
+	</head>
+	<body>
+		<button class="btn">Button</button>
+		<div class="dropdown">
+			<button class="btn">
+				<i class="fa fa-caret-down"></i>
+			</button>
+			<div class="dropdown-content">
+				<a href="#">Link 1</a>
+				<a href="#">Link 2</a>
+				<a href="#">Link 3</a>
+				<a href="#">Link 4</a>
+			</div>
+		</div>
+		<script src="js/main.js">
+		</script>
+	</body>
+</html>
